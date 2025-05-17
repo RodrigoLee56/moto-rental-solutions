18c311e987d0cdc084368c3e00984952e0192e55
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-05-17 00:30:04 -0400

    cria porjeto backend com java openjdk-21 e spring-boot

---
+++ backend/motorental-backend/src/main/java/com/motorental/motorental_backend/MotorentalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.motorental.motorental_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorentalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorentalBackendApplication.class, args);
+	}
+
+}
---
+++ backend/motorental-backend/src/test/java/com/motorental/motorental_backend/MotorentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorental.motorental_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorentalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
